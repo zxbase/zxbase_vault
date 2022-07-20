620f867218833dcfde7f35d21a47cb5e07f7e7da
Ariel Tubaltsev <ariel@zxbase.com>
2022-07-19 17:33:47 -0700

    add dart package skeleton

---
+++ example/zxbase_vault_example.dart
@@ -0,0 +1,6 @@
+import 'package:zxbase_vault/zxbase_vault.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/zxbase_vault.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library zxbase_vault;
+
+export 'src/zxbase_vault_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
