83151478d9b1afddaa5069f11b07e3a7bd8a692a
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-04-19 18:35:36 -0700

    update packages and version

--- lib/zxbase_vault.dart
+++ lib/zxbase_vault.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library zxbase_vault;
+library;
 
 export 'src/doc.dart';
 export 'src/doc/doc_meta.dart';
